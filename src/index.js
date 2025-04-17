720f5deed8b8d3dd4ab29cc9b829ee433dcde8d6
RisingToTheSky <141631716+RisingToTheSky@users.noreply.github.com>
2025-04-17 21:23:24 +0200

    Add Breadth-First-Search Algorithm

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-function setUpAdjacencyList() {
-    let up = 1;
-    let down = -1;
-    let left = -1;
-    let right = 1;
-    let rows = 8;
-    let columns = 8;
-    let adjacencyList = [];
+function setUpAdjacencyMap() {
+    const up = 1;
+    const down = -1;
+    const left = -1;
+    const right = 1;
+    const rows = 8;
+    const columns = 8;
+    const adjacencyMap = new Map();
     let board = [];
 
     let directions = [[left - 1, up], 
@@ -27,17 +27,44 @@ function setUpAdjacencyList() {
                     possibleMoves.push([row + directions[direction][0], column + directions[direction][1]]);
                 }
             }
-            adjacencyList.push(possibleMoves);
+            adjacencyMap.set([`${row}`, `${column}`].join("::"), possibleMoves);
         }
     }
 
-    console.log(board);
-    return adjacencyList;
+    return adjacencyMap;
+}
+
+function keyToString(key) {
+    return [`${key[0]}`, `${key[1]}`].join("::");
+}
+
+function breadthFirstSearch(root, map) {
+    let queue = [];
+    let visited = new Set();
+
+    queue.push(root);
+    visited.add(root);
+    while (queue.length != 0) {
+        let currentKey = queue.shift();
+        let stringKey = keyToString(currentKey);
+        let neighbors = map.get(stringKey);
+        for (let i = 0; i < neighbors.length; i++) {
+            if (!visited.has(neighbors[i])) {
+                queue.push(neighbors[i]);
+                visited.add(neighbors[i]);
+            }
+        }
+    }
+
+    return visited;
 }
 
 function knightMoves(start, end) {
+    let adjacencyMap = setUpAdjacencyMap();
+    let search = breadthFirstSearch([0,0], adjacencyMap);
+    console.log(search);
     return start;
 }
 
-console.log(setUpAdjacencyList());
+console.log(setUpAdjacencyMap());
 console.log(knightMoves([0,0], [3,3]));
